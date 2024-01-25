94106f0766c38e39e27bc6484113801cd1bd890e
Richard Barbour <richyb07@gmail.com>
2024-01-25 13:01:55 -0800

    Create basic react app with grapesjs library and preset newsletter plugin.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PresetNewsletter from './components/PresetNewsletter.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="gjs"></div>
+      <PresetNewsletter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PresetNewsletter.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import grapesjs from 'grapesjs';
+import plugin from 'grapesjs-preset-newsletter';
+import GjsEditor from '@grapesjs/react';
+
+const PresetNewsletter = () => {
+
+    //Initializes the preset-newsletter
+    const editor = grapesjs.init({
+    container : '#gjs',
+    plugins: [plugin],
+    pluginsOpts: {
+      [plugin]: { /* options */ }
+    },
+  });
+
+  return (
+    <>
+      <GjsEditor
+        // Pass the core GrapesJS library to the wrapper (required).
+        grapesjs={grapesjs}
+        // Load the GrapesJS CSS file asynchronously from URL.
+        // This is an optional prop, you can always import the CSS directly in your JS if you wish.
+        grapesjsCss="https://unpkg.com/grapesjs/dist/css/grapes.min.css"
+        // GrapesJS init options
+        options={{
+          height: '100vh',
+          storageManager: false,
+        }}
+        onEditor={editor}
+      />
+    </>
+    
+  );
+}
+
+export default PresetNewsletter
