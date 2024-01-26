5d99d182a908083ada86d3dcfd6533c7102960ae
Richard Barbour <richyb07@gmail.com>
2024-01-25 17:05:50 -0800

    WIP troubleshoot error messages re. no export Editor in grapesjs.

--- src/components/PresetNewsletter.js
+++ src/components/PresetNewsletter.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import grapesjs from 'grapesjs';
+import grapesjs, {Editor} from 'grapesjs';
 import plugin from 'grapesjs-preset-newsletter';
 import GjsEditor from '@grapesjs/react';
 
 const PresetNewsletter = () => {
 
     //Initializes the preset-newsletter
-    const editor = grapesjs.init({
-    container : '#gjs',
-    plugins: [plugin],
-    pluginsOpts: {
-      [plugin]: { /* options */ }
-    },
-  });
+  //   const editor = grapesjs.init({
+  //   container : '#gjs',
+  //   plugins: [plugin],
+  //   pluginsOpts: {
+  //     [plugin]: { /* options */ }
+  //   },
+  // });
+
 
   return (
     <>
@@ -22,12 +23,14 @@ const PresetNewsletter = () => {
         // Load the GrapesJS CSS file asynchronously from URL.
         // This is an optional prop, you can always import the CSS directly in your JS if you wish.
         grapesjsCss="https://unpkg.com/grapesjs/dist/css/grapes.min.css"
+        container='#gjs'
+        plugins={[plugin]}
         // GrapesJS init options
         options={{
           height: '100vh',
           storageManager: false,
         }}
-        onEditor={editor}
+        //onEditor={Editor}
       />
     </>
     
